b2ba88d9876a24885bbae3667bb15d36b6f13632
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2022-11-18 23:25:03 +0300

    completed Assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+function add(a,b){
+    return a+b ;
+
+}
+add(7,2);
+
+function subtract(a , b){
+    return a-b;
+}
+subtract(7,2);
+
+function multiply(a,b){
+    return a*b;
+
+}
+multiply(7,2)
+
+function divide(a,b){
+    return a/b;
+
+}
+multiply(8,2);
+
+
+
+
+
+
+function increment(number){
+    return (number +=1);
+}
+increment(10);
+
+function decrement(number){
+    return (number-=1);
+}
+decrement(7);
+
+function makeInt( number){
+return parseInt(number,10);
+
+}
+makeInt("2" );
+
+function preserveDecimal(number){
+    return parseFloat (number);
+
+}
+preserveDecimal(2.333);
+
